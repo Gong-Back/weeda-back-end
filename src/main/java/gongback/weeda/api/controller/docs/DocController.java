31450cca9ca7bb16687568e6f8a67eee94ad9940
YC <gks54410@gmail.com>
2023-01-14 00:09:17 +0900

    WEEDA-55 [fix]: api-docs 로직 수정

--- src/main/java/gongback/weeda/api/controller/docs/DocController.java
+++ src/main/java/gongback/weeda/api/controller/docs/DocController.java
@@ -2,8 +2,8 @@ package gongback.weeda.api.controller.docs;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,27 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api-docs")
 public class DocController {
 
-    private final FileSystemResourceLoader fileSystemResourceLoader;
+    private final ResourceLoader resourceLoader;
 
-    private final String PROFILE_INFO = "spring.profiles.active";
-    private final String LOCAL = "local";
+    @Value("${apiDocs.filePath}")
+    private String docsHtml;
 
-    @Value("${apiDocs.localFilePath}")
-    private String localDocsHtml;
-
-    @Value("${apiDocs.prodFilePath}")
-    private String prodDocsHtml;
-
-    public DocController() {
-        this.fileSystemResourceLoader = new FileSystemResourceLoader();
+    public DocController(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
     }
 
     @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
     public Resource docs() {
-        if (System.getProperty(PROFILE_INFO).equalsIgnoreCase(LOCAL)) {
-            return fileSystemResourceLoader.getResource(localDocsHtml);
-        }
-
-        return fileSystemResourceLoader.getResource(prodDocsHtml);
+        return resourceLoader.getResource(docsHtml);
     }
 }
