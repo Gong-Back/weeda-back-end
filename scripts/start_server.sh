5e2e5a74ff58a94b7b88939c8342b224f151ae49
YC <gks54410@gmail.com>
2023-01-13 20:52:29 +0900

    WEEDA-53 [chore]: 스크립트 변경

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PROJECT_NAME="weeda"
 JAR_PATH="/home/ec2-user/$PROJECT_NAME/build/libs/*-SNAPSHOT.jar"
+LIBS_PATH="build/libs/"
 DEPLOY_PATH=/home/ec2-user/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ec2-user/$PROJECT_NAME/log/deploy.log" # Deploy Log 파일
 DEPLOY_ERR_LOG_PATH="/home/ec2-user/$PROJECT_NAME/log/deploy_err.log" # Deploy Error Log 파일
@@ -8,13 +9,12 @@ APPLICATION_LOG_PATH="/home/ec2-user/$PROJECT_NAME/log/application.log" # Applic
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
 
-mkdir DEPLOY_PATH/log
+cd DEPLOY_PATH
+mkdir log
 
 echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
 echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
-echo "> build 파일 복사" >> %DEPLOY_LOG_PATH
-cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 동작 중인 애플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -30,7 +30,7 @@ else
   kill -9 $CURRENT_PID
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+DEPLOY_JAR=$LIBS_PATH$JAR_NAME
 echo "> $DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
 nohup java -jar -Dspring.profiles.active=prod $DEPLOY_JAR --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH & # & -> background exec
 
