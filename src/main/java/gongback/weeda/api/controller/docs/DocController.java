cc80ad12b4d7c192c335f19d924d8b8e17d6d501
youngchan <gks54410@gamil.com>
2023-01-13 20:03:51 +0900

    WEEDA-55 [fix]: api-docs 로직 수정

--- src/main/java/gongback/weeda/api/controller/docs/DocController.java
+++ src/main/java/gongback/weeda/api/controller/docs/DocController.java
@@ -1,21 +1,34 @@
 package gongback.weeda.api.controller.docs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
+@Slf4j
 @RestController
 @RequestMapping("/api-docs")
 public class DocController {
 
-    @Value("classpath:/static/docs/api-docs.html")
-    Resource html;
+    private final ResourceLoader resourceLoader;
+
+    @Value("${apiDocs.filePath}")
+    private String docsHtml;
+
+    public DocController(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
-    public Resource docs() {
-        return html;
+    public Resource docs() throws IOException {
+        log.info(resourceLoader.getResource("classpath:" + docsHtml).getURL().toString());
+        log.info(resourceLoader.getResource("classpath:").getURL().toString());
+        return resourceLoader.getResource("classpath:" + docsHtml);
     }
 }
