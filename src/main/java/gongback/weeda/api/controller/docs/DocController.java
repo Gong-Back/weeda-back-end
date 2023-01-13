8fc434e9317a4b740eca8e0d79e2ae7134cb836e
YC <gks54410@gmail.com>
2023-01-13 23:00:18 +0900

    WEEDA-55 [fix]: api-docs 로직 수정

--- src/main/java/gongback/weeda/api/controller/docs/DocController.java
+++ src/main/java/gongback/weeda/api/controller/docs/DocController.java
@@ -2,31 +2,39 @@ package gongback.weeda.api.controller.docs;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @Slf4j
 @RestController
 @RequestMapping("/api-docs")
 public class DocController {
 
-    private final ResourceLoader resourceLoader;
+    private final FileSystemResourceLoader fileSystemResourceLoader;
 
-    @Value("${apiDocs.filePath}")
-    private String docsHtml;
+    private final String PROFILE_INFO = "spring.profiles.active";
+    private final String LOCAL = "local";
 
-    public DocController(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
+    @Value("${apiDocs.localFilePath}")
+    private String localDocsHtml;
+
+    @Value("${apiDocs.prodFilePath}")
+    private String prodDocsHtml;
+
+    public DocController() {
+        this.fileSystemResourceLoader = new FileSystemResourceLoader();
     }
 
     @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
-    public Resource docs() throws IOException {
-        return resourceLoader.getResource("classpath:" + docsHtml);
+    public Resource docs() {
+        if (System.getProperty(PROFILE_INFO).equalsIgnoreCase(LOCAL)) {
+            return fileSystemResourceLoader.getResource(localDocsHtml);
+        }
+
+        return fileSystemResourceLoader.getResource(prodDocsHtml);
     }
 }
