a8cfdea69bd2df241d3e922d1102acbff08ccc93
YC <gks54410@gmail.com>
2023-01-14 00:30:28 +0900

    WEEDA-55 [fix]: api-docs 로직 수정

--- src/main/java/gongback/weeda/api/controller/docs/ProdDocController.java
+++ src/main/java/gongback/weeda/api/controller/docs/ProdDocController.java
@@ -2,8 +2,9 @@ package gongback.weeda.api.controller.docs;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@Profile("prod")
 @RequestMapping("/api-docs")
-public class DocController {
+public class ProdDocController {
 
-    private final ResourceLoader resourceLoader;
+    private final FileSystemResourceLoader resourceLoader;
 
-    @Value("${apiDocs.filePath}")
-    private String docsHtml;
+    @Value("${apiDocs.prodFilePath}")
+    private String prodDocsHtml;
 
-    public DocController(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
+    public ProdDocController() {
+        this.resourceLoader = new FileSystemResourceLoader();
     }
 
     @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
     public Resource docs() {
-        return resourceLoader.getResource(docsHtml);
+        return resourceLoader.getResource(prodDocsHtml);
     }
 }
