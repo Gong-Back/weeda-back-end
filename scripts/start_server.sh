c07cbcf11827949f9958d18225c419a091241e8b
YC <gks54410@gmail.com>
2023-01-13 21:41:39 +0900

    WEEDA-53 [chore]: 스크립트 변경

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJECT_NAME="weeda"
 JAR_PATH="/home/ec2-user/$PROJECT_NAME/build/libs/*-SNAPSHOT.jar"
-LIBS_PATH="build/libs/"
+BUILD_LIBS_PATH="/home/ec2-user/$PROJECT_NAME/build/libs/"
 DEPLOY_PATH=/home/ec2-user/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ec2-user/$PROJECT_NAME/log/deploy.log" # Deploy Log 파일
 DEPLOY_ERR_LOG_PATH="/home/ec2-user/$PROJECT_NAME/log/deploy_err.log" # Deploy Error Log 파일
@@ -10,7 +10,6 @@ BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
 
 mkdir DEPLOY_PATH/log
-cd DEPLOY_PATH
 
 echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
@@ -30,7 +29,7 @@ else
   kill -9 $CURRENT_PID
 fi
 
-DEPLOY_JAR=$LIBS_PATH$JAR_NAME
+DEPLOY_JAR=BUILD_LIBS_PATH$JAR_NAME
 echo "> $DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
 nohup java -jar -Dspring.profiles.active=prod $DEPLOY_JAR --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH & # & -> background exec
 
